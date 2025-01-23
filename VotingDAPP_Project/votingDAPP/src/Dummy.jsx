742eb56ae2061405622078bdc60196c2d570cc77
abhi <abhi.skamboj@gmail.com>
2025-01-23 10:37:10 +0530

    all Added to Second branch

--- VotingDAPP_Project/votingDAPP/src/Dummy.jsx
+++ VotingDAPP_Project/votingDAPP/src/Dummy.jsx
@@ -1,10 +1,10 @@
-import { createContext } from "react";
+import {   useContext } from "react";
 import { Web3context } from "./context/web3context";
 
 
 
 export default function Dummy() {
-    const {contractInstance,selectedAccount,chainId}=createContext(Web3context);
+    const {contractInstance,selectedAccount,chainId}=useContext(Web3context);
     console.log(contractInstance,selectedAccount,chainId);
   return (
     <div>Dummy</div>
